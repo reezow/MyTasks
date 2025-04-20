44c5e14303a3ef03328dc3b9ea249ac8c11fe2c1
reezow <reeceaidanfarmer@gmail.com>
2025-04-20 20:36:14 +0200

    Defined the model class and setup a Db context - store the data using EF Core In-Memory db

---
+++ MyTasksWeb/Data/Tasks.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTasksWeb.Data
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+
+        [Display (Name = "What To Do?")]
+        public string? Title { get; set; }
+
+        [Display(Name = "Am I done?")]
+        public bool isCompleted { get; set; }
+    }
+}
--- MyTasksWeb/Program.cs
+++ MyTasksWeb/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyTasksWeb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseInMemoryDatabase("TasksDb")
+);
 
 var app = builder.Build();
 
